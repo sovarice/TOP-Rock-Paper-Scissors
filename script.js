07b094e36d035bef0124f7a13656f86266743e47
sovarice <super.rentagrid@yandex.ru>
2023-02-01 21:53:18 +0300

    Fleshed out the logic of the game

---
+++ script.js
@@ -0,0 +1,10 @@
+// Computer will play first
+// A function getComputerChoice() will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+// Now it's the player's turn
+// A function playRound() will take two parameters: playerSelection and computerSelection 
+// playerSelection will prompt the user to enter "Rock, Paper or Scissors"
+// playRound() will run conditions:
+// 		IF playerSelection strictly equals getComputerChoice() THEN playRound() returns a string "It's a tie!"
+// A function playRound() will return a string that declares the winner: "You Lose! Paper beats Rock"
+// A new function game() will loop function playRound() 5 times and declare winner and loser
+//
