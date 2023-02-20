7d39f6dc35d6c386f57328b570e9df6c18e8977a
sovarice <super.rentagrid@yandex.ru>
2023-02-20 14:56:35 +0300

    Fixed script fallback to index.html

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+const game = () => {
+	let pScore = 0;
+	let cScore = 0;
+	
+	const playRound = () => {
+	const options = ['rock', 'paper', 'scissors'];
+
+	const computerNumber = Math.floor(Math.random() * 3);
+	console.log(computerNumber);
+	};
+	playRound();
+};
 // Computer will play first
 // A function getComputerChoice() will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+
 // Now it's the player's turn
 // A function playRound() will take two parameters: playerSelection and computerSelection 
 // playerSelection will prompt the user to enter "Rock, Paper or Scissors"
@@ -8,3 +21,4 @@
 // A function playRound() will return a string that declares the winner: "You Lose! Paper beats Rock"
 // A new function game() will loop function playRound() 5 times and declare winner and loser
 //
+game ();
